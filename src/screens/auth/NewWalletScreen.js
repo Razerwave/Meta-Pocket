4bf7709193dc46fa0cdc201e0f1663a50732e8c9
shijir0330 <eastwindshijir@gmail.com>
2023-01-20 14:41:34 +0800

    functions are working

--- src/screens/auth/NewWalletScreen.js
+++ src/screens/auth/NewWalletScreen.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, Button, Alert, StyleSheet } from 'react-native'
 import { ROUTES } from '../../constants'
+import Clipboard from '@react-native-clipboard/clipboard';
 
 const NewWalletScreen = ({ navigation }) => {
+  const [copied, setCopied] = useState(false);
+  const [words, setWords] = useState('')
+
+  useEffect(() => {
+    setWords('Isolate tiny hat want latin glide already Friend exchange gap frost inner')
+  }, [])
+
+  const handleCopy = () => {
+    Clipboard.setString(words)
+    setCopied(true)
+  }
+
   return (
     <View>
-      <Text>NewWalletScreen</Text>
-      <Button title='Continue' onPress={() => navigation.navigate(ROUTES.NEW_WALLET_WORDS)} />
+      <Text>Back Up Your Wallet</Text>
+      <Text>The recovery phrase is the only way to recover your cryptocurrency if you lose your phone or switch to another wallet.</Text>
+      <Text>Keep the 12-word recovery phrase in a safe place and don’t share it with anyone.</Text>
+      <View>
+        <Text>{words}</Text>
+      </View>
+      <Button title={copied ? 'Copied' : 'Copy'} onPress={handleCopy} />
+      <Button title='Continue' onPress={() => navigation.navigate(ROUTES.NEW_WALLET_WORDS, { words })} />
     </View>
   )
 }
--- src/screens/auth/NewWalletWordScreen.js
+++ src/screens/auth/NewWalletWordScreen.js
@@ -1,11 +1,34 @@
 import React, { useState } from 'react'
-import { View, Text, Button, Alert, StyleSheet } from 'react-native'
+import { View, Text, Button, Alert, StyleSheet, TouchableHighlight } from 'react-native'
+import { ROUTES } from '../../constants'
 
-const NewWalletWordScreen = ({ navigation }) => {
-  const passed = false
+const NewWalletWordScreen = ({ route, navigation }) => {
+  const { words } = route.params
+  const wordsArray = words.split(' ')
+  const [chosenWords, setChosenWords] = useState([])
+
+  const handleClick = (word) => {
+    chosenWords.includes(word) 
+    ? setChosenWords(arr => arr.filter(v => v !== word))
+    : setChosenWords(arr => ([...arr, word]))
+  }
+  
+  const passed = chosenWords.join(' ') === words
   return (
     <View>
-      <Text>NewWalletWordScreen</Text>
+      <Text>Have you backed up?</Text>
+      <Text>Please click the first(1st) word first, then the last word(12th)</Text>
+      <Text>{words}</Text>
+      <Text>{chosenWords.join(' ')}</Text>
+      <View>
+        {wordsArray.map((word, index) => (
+          <TouchableHighlight key={index} onPress={() => handleClick(word)}>
+            <View>
+              <Text>{word} : {chosenWords.findIndex(v => v === word)+1}</Text>
+            </View>
+          </TouchableHighlight>
+        ))}
+      </View>
       <Button title='Continue' disabled={!passed} onPress={() => navigation.navigate(ROUTES.NEW_WALLET_WORDS)} />
     </View>
   )

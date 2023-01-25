21801511d54b98abf735e9c6e902d8826bad399a
shijir0330 <eastwindshijir@gmail.com>
2023-01-25 11:10:54 +0800

    add on settings

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 import {View, Text, Button, Alert, StyleSheet} from 'react-native'
+import { ButtonPrimary } from '../components'
+import { useThemeChange } from '../context/ThemeChangeContext'
 
 const SettingScreen = () => {
+  const { theme, toggleTheme } = useThemeChange()
+
   return (
     <View>
       <Text>SettingScreen</Text>
+      <ButtonPrimary title={`Theme Toggle: ${theme}`} onPress={() => toggleTheme()} />
     </View>
   )
 }
--- src/screens/auth/WelcomeScreen.js
+++ src/screens/auth/WelcomeScreen.js
@@ -5,14 +5,10 @@ import { ROUTES } from '../../constants'
 import { useThemeChange } from '../../context/ThemeChangeContext'
 
 const WelcomeScreen = ({ navigation }) => {
-  const { theme, toggleTheme } = useThemeChange()
-
-
   return (
     <Screen bottom={[
       <ButtonPrimary key="1" title='Create New Wallet' onPress={() => navigation.navigate(ROUTES.NEW_WALLET)} />,
       <ButtonPrimary key="2" title='I already have a wallet' onPress={() => navigation.navigate(ROUTES.RECOVER_WALLET)} />,
-      <ButtonPrimary key="3" title={`Theme Toggle: ${theme}`} onPress={() => toggleTheme()} />
     ]}>
     </Screen>
   )

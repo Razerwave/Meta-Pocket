aa3d60aa9c5fc10f1e847586add6eb1309d54ef5
shijir <eastwindshijir@gmail.com>
2023-02-01 09:01:30 +0800

    StatusBar

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
 import ContextProvider from './src/context/ContextProvider';
 import TheNavigation, { MainNavigation } from './src/navigations';
 // import styled from 'styled-components/native'
 
 const App = () => {
+  // #f8f9fa
+  // #343a40
   return (
     <ContextProvider>
-      {/* <MainNavigation /> */}
+      <TheStatusBar />
       <TheNavigation />
     </ContextProvider>
   );
 }
-// const Wrapper = styled.View`
-//   background-color: green;
-// `;
+
+import { useTheme } from 'styled-components'
+import { useThemeChange } from './src/context/ThemeChangeContext';
+const TheStatusBar = () => {
+  const { backgroundColor } = useTheme()
+  const { theme } = useThemeChange()
+
+  return (
+    <StatusBar
+      backgroundColor={backgroundColor}
+      barStyle={theme === 'dark' ? 'light-content' : 'dark-content'}
+      showHideTransition={'slide'}
+      hidden={false}
+    />
+  )
+}
 
 export default App;

882795ef98862f5c428bc65ef857f6b7a8195e62
Khatansaihan Erdenebileg <hatnaa.cs@gmail.com>
2023-02-09 15:53:58 +0800

    explore

--- src/components/CardBox.js
+++ src/components/CardBox.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 
 const CardBox = styled.View`
-    padding: 16px 16px 26px 16px;
-    background-color: ${props => props.theme.backgroundCardColor};
-`
+  padding: 16px 16px 26px 16px;
+  background-color: ${props => props.theme.backgroundCardColor};
+`;
 
-export default CardBox
+export default CardBox;
--- src/screens/Explore/ExploreListScreen.js
+++ src/screens/Explore/ExploreListScreen.js
@@ -1,55 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, Modal } from 'react-native';
-import { CardBox, HomeScreen, Screen, Stack, StyledText } from '../../components';
-import { useTheme } from 'styled-components';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TouchableOpacity, Modal} from 'react-native';
+import {CardBox, HomeScreen, Screen, Stack, StyledText} from '../../components';
+import {useTheme} from 'styled-components';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import WebView from 'react-native-webview';
-import { ROUTES } from '../../constants';
+import {ROUTES} from '../../constants';
 
-const ExploreListScreen = ({ navigation, route }) => {
-  const { fontColor, activeTintColor } = useTheme();
+const ExploreListScreen = ({navigation, route}) => {
+  const {fontColor, activeTintColor} = useTheme();
   const data = route.params.data;
   const title = route.params.title;
   const WEB_LINK = 'https://www.youtube.com/';
-
+  console.log(data, ' ++++ ', title);
   return (
     <HomeScreen>
       <Stack spacing={16} padding={16}>
-        {data.map(index => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate(ROUTES.EXPLORE.WEB, { uri: WEB_LINK })}
-            key={index.Subject}
-            fontColor={fontColor}
-            style={{ flexDirection: 'row', gap: 16 }}>
-            <View
-              style={{
-                height: 40,
-                width: 40,
-                borderWidth: 1,
-                borderColor: fontColor,
-              }}></View>
-            <Stack style={{ flex: 1 }}>
-              <Stack
-                direction="row"
-                style={{ borderWidth: 1, borderColor: 'gray' }}>
-                <StyledText style={{ fontWeight: 'bold', paddingHorizontal: 8 }}>
-                  {index.Subject}
-                </StyledText>
-              </Stack>
+        {data.map(index =>
+          console.log(index)(
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate(ROUTES.EXPLORE.WEB, {uri: WEB_LINK})
+              }
+              key={index.Subject}
+              fontColor={fontColor}
+              style={{flexDirection: 'row', gap: 16}}>
+              <View
+                style={{
+                  height: 40,
+                  width: 40,
+                  borderWidth: 1,
+                  borderColor: fontColor,
+                }}></View>
+              <Stack style={{flex: 1}}>
+                <Stack
+                  direction="row"
+                  style={{borderWidth: 1, borderColor: 'gray'}}>
+                  <StyledText
+                    style={{fontWeight: 'bold', paddingHorizontal: 8}}>
+                    {index.Subject}
+                  </StyledText>
+                </Stack>
 
-              <Stack>
-                <StyledText
-                  style={{
-                    borderWidth: 1,
-                    borderColor: 'gray',
-                    paddingHorizontal: 8,
-                  }}>
-                  {index.Description}
-                </StyledText>
+                <Stack>
+                  <StyledText
+                    style={{
+                      borderWidth: 1,
+                      borderColor: 'gray',
+                      paddingHorizontal: 8,
+                    }}>
+                    {index.Description}
+                  </StyledText>
+                </Stack>
               </Stack>
-            </Stack>
-          </TouchableOpacity>
-        ))}
+            </TouchableOpacity>,
+          ),
+        )}
       </Stack>
     </HomeScreen>
   );

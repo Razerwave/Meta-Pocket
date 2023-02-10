93b05ddd6f8a0ed6747c6599015e12863c85b792
shijir <eastwindshijir@gmail.com>
2023-02-10 14:17:44 +0800

    keyboard back key

--- src/components/KeyBoardPasscode.js
+++ src/components/KeyBoardPasscode.js
@@ -3,8 +3,15 @@ import React, { useEffect, useState } from 'react'
 import Stack from './Stack'
 import StyledText from './texts/StyledText'
 import { white } from '../constants/colors'
+import { SvgXml } from 'react-native-svg'
 
 const DEFAULT_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+const BACKKEY_XML = `<svg width="41" height="20" viewBox="0 0 41 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.86234 1.36339C8.62184 0.49689 9.71813 0 10.8704 0H36.5C38.7091 0 40.5 1.79086 40.5 4V10V16C40.5 18.2091 38.7091 20 36.5 20H10.791C9.68495 20 8.62829 19.542 7.87206 18.7349L2.47865 12.9787C1.07207 11.4775 1.03352 9.15421 2.38953 7.60718L7.86234 1.36339Z" fill="#F2F2F2" fill-opacity="0.1"/>
+<rect x="19" y="13.2728" width="10.2852" height="1.02852" rx="0.514258" transform="rotate(-45 19 13.2728)" fill="#F2F2F2"/>
+<rect x="26.2725" y="14" width="10.2852" height="1.02852" rx="0.514258" transform="rotate(-135 26.2725 14)" fill="#F2F2F2"/>
+</svg>
+`
 
 const KeyBoardPasscode = ({ value = '', onChange = () => { } }) => {
   const [numbers, setNumbers] = useState([])
@@ -47,13 +54,13 @@ const KeyBoardPasscode = ({ value = '', onChange = () => { } }) => {
       </View>
       <View style={styles.keyContainer}>
         <TouchableOpacity style={styles.key} onPress={() => handleReset()}>
-          <StyledText>RESET</StyledText>
+          <StyledText>Reset</StyledText>
         </TouchableOpacity>
         <TouchableOpacity style={styles.key} onPress={() => handleNumber(numbers[9])}>
           <StyledText>{numbers[9]}</StyledText>
         </TouchableOpacity>
         <TouchableOpacity style={styles.key} onPress={() => handleDelete()}>
-          <StyledText>DELETE</StyledText>
+          <SvgXml xml={BACKKEY_XML}/>
         </TouchableOpacity>
       </View>
     </View>
@@ -89,5 +96,9 @@ const styles = StyleSheet.create({
     paddingVertical: 18,
     marginBottom: 4,
     alignItems: 'center',
+
+    fontFamily: 'Lato',
+    fontSize: 14,
+    lineHeight: 17,
   }
 })

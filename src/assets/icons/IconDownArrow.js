08f6ac368151e9374977bc62c5e377c43ca50ffc
Nano <nanoshdee@gmail.com>
2023-02-16 16:05:48 +0800

    select improvement

--- src/assets/icons/IconDownArrow.js
+++ src/assets/icons/IconDownArrow.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { useTheme } from 'styled-components';
-import { Path, Svg } from 'react-native-svg';
+import {useTheme} from 'styled-components';
+import {Path, Svg} from 'react-native-svg';
 
-const IconArrowDown = () => {
-  const { fontColor } = useTheme();
+const IconArrowDown = ({color}) => {
+  const {fontColor} = useTheme();
   return (
-    <Svg width="8" height="6" viewBox="0 0 8 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <Path d="M4 6L0.535898 6.52533e-07L7.4641 4.68497e-08L4 6Z" fill={fontColor} />
+    <Svg
+      width="8"
+      height="8"
+      viewBox="0 0 8 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <Path
+        d="M4 6L0.535898 6.52533e-07L7.4641 4.68497e-08L4 6Z"
+        fill={color || fontColor}
+      />
     </Svg>
-  )
-}
+  );
+};
 
-export default IconArrowDown
+export default IconArrowDown;

a25dae72d0a97bddfec4525f85ee3946b2ee697e
shijir <eastwindshijir@gmail.com>
2023-02-17 13:37:25 +0800

    animation, passcode center

--- src/assets/animations/index.js
+++ src/assets/animations/index.js
@@ -1,5 +1,7 @@
 import AnimationLoading from "./AnimationLoading";
+import AnimationCompleted from "./AnimationCompleted";
 
 export { 
   AnimationLoading,
+  AnimationCompleted,
 }
--- src/screens/auth/Result.js
+++ src/screens/auth/Result.js
@@ -1,24 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
-import { ButtonPrimary, LayoutBottom, LayoutScreen } from '../../components'
-import { Layout } from 'react-native-reanimated'
+import { ButtonPrimary, LayoutBottom, LayoutCenter, LayoutScreen, Stack } from '../../components'
 import { useAuth } from '../../context/AuthContext'
+import { AnimationCompleted } from '../../assets/animations'
 
 const Result = ({ route }) => {
   const { i18n, login } = useAuth()
 
   return (
     <LayoutScreen>
-      <LayoutBottom>
+      <View style={styles.upper}>
+        <AnimationCompleted />
+      </View>
+      <View style={styles.bottom}>
         <ButtonPrimary
           title={i18n.getStarted}
           onPress={() => login(route.params.passcode)}
         />
-      </LayoutBottom>
+      </View>
     </LayoutScreen>
   )
 }
 
 export default Result
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  upper: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingRight: 40,
+    paddingLeft: 45,
+  },
+  bottom: {
+    height: 180,
+    alignItems: 'center',
+  }
+})

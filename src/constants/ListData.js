82bad01491fece6f3d2efecdb14463ade77fd2bc
shijir <eastwindshijir@gmail.com>
2023-02-17 10:53:27 +0800

    notice list

--- src/constants/ListData.js
+++ src/constants/ListData.js
@@ -1,10 +1,7 @@
 import {
   gray100,
-  gray200,
-  gray300,
   green400,
   magenta,
-  neutral100,
   primary,
   purple100,
   red,
@@ -127,104 +124,6 @@ const ExploreData = {
     },
   ],
 };
-
-// const Artlinks = [
-//   {
-//     name: 'Two gils ...',
-//     author: 'Pierre-Auguste Renoir (1881)',
-//     coin: 'ETH',
-//     total: '0.1',
-//     image: require('../assets//icons/MaskGroup.png'),
-//     fav: true,
-//   },
-//   {
-//     name: 'Flowers ...',
-//     author: 'Edgar Degas (1873)',
-//     coin: 'ETH',
-//     total: '0.3',
-//     image: require('../assets//icons/flowers.png'),
-//     fav: true,
-//   },
-//   {
-//     name: 'Steamboats in the Port ...',
-//     author: 'Vladimir Kush (1111)',
-//     coin: 'ETH',
-//     total: '0.2',
-//     image: require('../assets//icons/SteamBoats.png'),
-//     fav: true,
-//   },
-//   {
-//     name: 'Ancient Winter city ...',
-//     author: 'Paul Jackson Pollock (1912)',
-//     coin: 'ETH',
-//     total: '0.2',
-//     image: require('../assets//icons/Rectangle229.png'),
-//     fav: true,
-//   },
-// ];
-
-// const links = [
-//   {
-//     id: 2,
-//     Subject: 'Aleo',
-//     url: 'Aleo.org',
-//     Image: require('../assets/icons/all.png'),
-//     logo: require('../assets/icons/Group6679.png'),
-//     fav: false,
-//   },
-//   {
-//     id: 1,
-//     Subject: 'Bitcoin',
-//     url: 'bitcoin.org/ko',
-//     Image: require('../assets/icons/Vector.png'),
-//     logo: require('../assets/icons/bitcoin.png'),
-//     fav: true,
-//   },
-//   {
-//     id: 3,
-//     Subject: 'Filecoin',
-//     url: 'filecoin.io',
-//     Image: require('../assets/icons/Ellipse93.png'),
-//     logo: require('../assets/icons/fx.png'),
-//     fav: true,
-//   },
-//   {
-//     id: 4,
-//     Subject: 'Plenty',
-//     url: 'www.plenty.com',
-//     Image: require('../assets/icons/Ellipse94.png'),
-//     logo: require('../assets/icons/Layer_1.png'),
-//     fav: true,
-//   },
-//   {
-//     id: 5,
-//     Subject: 'Ethererium',
-//     url: 'www.etherium.com',
-//     Image: require('../assets/icons/Ellipse95.png'),
-//     fav: false,
-//   },
-//   {
-//     id: 6,
-//     Subject: 'Sol',
-//     url: 'www.sol.io',
-//     Image: require('../assets/icons/Ellipse96.png'),
-//     fav: false,
-//   },
-//   {
-//     id: 7,
-//     Subject: 'Tether',
-//     url: 'www.Tether.org',
-//     Image: require('../assets/icons/favorite.png'),
-//     fav: true,
-//   },
-//   {
-//     id: 6,
-//     Subject: 'Ethererium1',
-//     url: 'www.etherium.com',
-//     Image: require('../assets/icons/Ellipse95.png'),
-//     fav: false,
-//   },
-// ];
 
 const items = [
   {
--- src/screens/setting/NoticeList.js
+++ src/screens/setting/NoticeList.js
@@ -1,15 +1,22 @@
+import { useEffect, useState } from 'react'
 import { StyleSheet, TouchableOpacity } from 'react-native'
 import { BodyText, CustomSearch, InvoiceSheet, LayoutScreen, LayoutScroll, Stack } from '../../components'
 import NoticeCard from '../../components/NoticeCard'
 import { ROUTES } from '../../constants'
 import { gray300 } from '../../constants/colors'
+import { WalletHomeData } from '../../constants/ListData'
 import { useAuth } from '../../context/AuthContext'
 
 const NoticeList = ({ navigation }) => {
   const { i18n } = useAuth();
+  const [noticeList, setNoticeList] = useState([])
+
+  useEffect(() => {
+    setNoticeList(WalletHomeData.notice)
+  }, [])
 
   const handleSearch = (e) => {
-    console.log("search", e.nativeEvent.text)
+    // console.log("search", e.nativeEvent.text)
   }
 
   return (
@@ -23,15 +30,15 @@ const NoticeList = ({ navigation }) => {
 
         <LayoutScroll>
           <Stack spacing={30}>
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((_, index) => (
+            {noticeList.map(({title, imagePath}, index) => (
               <TouchableOpacity key={index} onPress={() => navigation.navigate(ROUTES.SETTING.NOTICE, {
-                title: i18n.metaVerseComingSoon,
+                title: title,
               })}>
                 <Stack spacing={10}>
-                  <NoticeCard title={i18n.metaVerseComingSoon} />
+                  <NoticeCard imagePath={imagePath} title={title} />
                   <Stack spacing={6} marginHorizontal={10}>
                     <BodyText>
-                      {i18n.metaVerseComingSoon}
+                      {title}
                     </BodyText>
                     <BodyText type={5} style={{ color: gray300 }}>
                       Feb 7, 2023   |  조회수 2,300
